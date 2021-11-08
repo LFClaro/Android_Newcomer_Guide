414130d277355fefd2abecb39e03d3fa327f8134
LFClaro <lfclaro@gmail.com>
2021-11-08 14:17:21 -0500

    Layout v1 test

--- app/src/main/java/com/example/android1finalproject/MainActivity.java
+++ app/src/main/java/com/example/android1finalproject/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.android1finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView toolbar_menu = findViewById(R.id.toolbar_menu);
+        ImageView toolbar_logo = findViewById(R.id.toolbar_logo);
+        TextView toolbar_title = findViewById(R.id.toolbar_title);
+
+        toolbar_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Toast.makeText(MainActivity.this, "TODO Menu goes here", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
