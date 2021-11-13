17da3cb408c5ef77ae1e024783a79a3f9c51f411
LFClaro <lfclaro@gmail.com>
2021-11-12 19:54:36 -0500

    dropdown menu start

--- app/src/main/java/com/example/android1finalproject/MainActivity.java
+++ app/src/main/java/com/example/android1finalproject/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.android1finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,8 +24,31 @@ public class MainActivity extends AppCompatActivity {
         toolbar_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                Toast.makeText(MainActivity.this, "TODO Menu goes here", Toast.LENGTH_SHORT).show();
+                showMenu(v);
             }
         });
+    }
+
+    private void showMenu(View v){
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, v);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.community_menu)
+                    Toast.makeText(MainActivity.this, "TODO Community Menu goes here", Toast.LENGTH_SHORT).show();
+                if(menuItem.getItemId() == R.id.food_menu)
+                    Toast.makeText(MainActivity.this, "TODO Food Menu goes here", Toast.LENGTH_SHORT).show();
+                if(menuItem.getItemId() == R.id.health_menu)
+                    Toast.makeText(MainActivity.this, "TODO Health Menu goes here", Toast.LENGTH_SHORT).show();
+                if(menuItem.getItemId() == R.id.housing_menu)
+                    Toast.makeText(MainActivity.this, "TODO Housing Menu goes here", Toast.LENGTH_SHORT).show();
+                if(menuItem.getItemId() == R.id.govt_menu)
+                    Toast.makeText(MainActivity.this, "TODO Government Menu goes here", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        popupMenu.show();
     }
 }
