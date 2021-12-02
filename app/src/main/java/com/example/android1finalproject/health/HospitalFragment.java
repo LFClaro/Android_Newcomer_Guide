e0ee8f33eff7edf0c6c775e29d570aa32f9da411
Shanna2020 <shanna_nikki@hotmail.com>
2021-12-01 19:40:26 -0500

    Shanna Final Edit

--- app/src/main/java/com/example/android1finalproject/health/HospitalFragment.java
+++ app/src/main/java/com/example/android1finalproject/health/HospitalFragment.java
@@ -1,9 +1,12 @@
 package com.example.android1finalproject.health;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,18 +14,26 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.android1finalproject.R;
 import com.example.android1finalproject.databinding.FragmentHospitalBinding;
+import com.example.android1finalproject.housing.models.House;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class HospitalFragment extends Fragment {
 
     private FragmentHospitalBinding binding;
-
+EditText FindET;
+TextView displayTV;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
+//inflate fragment
         binding = FragmentHospitalBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -30,19 +41,51 @@ public class HospitalFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        FindET=  view.findViewById(R.id.healthSearchTxt);
+        FindET.setHint("Enter Hospital Name");
+        displayTV=view.findViewById(R.id.healthdisplayTxt);
         binding.Find.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(HospitalFragment.this)
-                        .navigate(R.id.action_HospitalFragment_to_FirstFragmentHealth);
+               //check if text is empty
+                if (FindET.getText().toString().isEmpty()) {
+                    FindET.setError("Required");
+                }else{
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("hospitals")
+                        .whereEqualTo("name", FindET.getText().toString())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d("Firestore", document.getId() + " => " + document.getData());
+                                        displayTV.setText("Name: " + document.getString("name") + "\nAddress: " + document.getString("address") + "\nNumber: " + document.getString("phone"));
+
+                                    }
+
+                                } else {
+
+                                    Log.d("Firestore", "Error getting documents: ", task.getException());
+
+                                }
+
+                            }
+
+                        });
+            }//end of else
+                displayTV.setText("Sorry, Cannot Find Hospital ");
             }
         });
-    }
+    }//On view create
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-}
+
+}//Fragment Create View
+
+
